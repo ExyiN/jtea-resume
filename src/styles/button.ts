eb8bf495507f54d59e8ee05023c84e8f8ddeffbe
Jacques Tea <jacquestea16@gmail.com>
2026-03-26 15:12:53 +0100

    change buttons style

--- src/styles/button.ts
+++ src/styles/button.ts
@@ -3,5 +3,14 @@ import { ButtonDesignTokens } from '@primeuix/themes/types/button';
 export const ButtonStyle: ButtonDesignTokens = {
   root: {
     borderRadius: '1rem',
+    paddingX: '1.5rem',
+    paddingY: '0.75rem'
   },
 };
+
+export const IconOnlyButtonStyle: ButtonDesignTokens = {
+  root: {
+    paddingX: '0.5rem',
+    paddingY: '0.5rem',
+  },
+}
