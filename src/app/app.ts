f360f1e41791797bf9679e1942d9de1f381d9dd8
Jacques Tea <jacquestea16@gmail.com>
2026-04-08 14:59:14 +0200

    add languages panel

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,15 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { Navbar } from './navbar/navbar';
+import { HardSkillsPage } from './pages/hard-skills-page/hard-skills-page';
 import { HomePage } from './pages/home-page/home-page';
+import { LanguagesPage } from './pages/languages-page/languages-page';
 import { ProfilePage } from './pages/profile-page/profile-page';
+import { SoftSkillsPage } from './pages/soft-skills-page/soft-skills-page';
 import { ThemeService } from './utils/theme-service';
-import { HardSkillsPage } from "./pages/hard-skills-page/hard-skills-page";
-import { SoftSkillsPage } from "./pages/soft-skills-page/soft-skills-page";
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, HomePage, ProfilePage, HardSkillsPage, SoftSkillsPage],
+  imports: [Navbar, HomePage, ProfilePage, HardSkillsPage, SoftSkillsPage, LanguagesPage],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ src/app/pages/languages-page/languages-page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { PanelModule } from 'primeng/panel';
+
+@Component({
+  selector: 'app-languages-page',
+  imports: [PanelModule],
+  templateUrl: './languages-page.html',
+  styleUrl: './languages-page.css',
+})
+export class LanguagesPage {}
