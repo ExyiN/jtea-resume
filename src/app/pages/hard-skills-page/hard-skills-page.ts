eb6126114f957e8756f4c631958a93fda8a3faaf
Jacques Tea <jacquestea16@gmail.com>
2026-04-15 12:53:12 +0200

    hard-skills translation

--- src/app/pages/hard-skills-page/hard-skills-page.ts
+++ src/app/pages/hard-skills-page/hard-skills-page.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { _, TranslatePipe, TranslateService, Translation } from '@ngx-translate/core';
 import { PanelModule } from 'primeng/panel';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-hard-skills-page',
-  imports: [PanelModule],
+  imports: [PanelModule, TranslatePipe, AsyncPipe],
   templateUrl: './hard-skills-page.html',
   styleUrl: './hard-skills-page.css',
 })
-export class HardSkillsPage {}
+export class HardSkillsPage {
+  private translate: TranslateService = inject(TranslateService);
+
+  protected getSkills(key: string): Observable<Translation> {
+    return this.translate.get(_(`hard-skills.${key}.items`));
+  }
+}
