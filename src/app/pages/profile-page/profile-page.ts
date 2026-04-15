9eeb498ecfa259506da72fc7f246c3f32471d423
Jacques Tea <jacquestea16@gmail.com>
2026-04-15 11:35:22 +0200

    profile translation

--- src/app/pages/profile-page/profile-page.ts
+++ src/app/pages/profile-page/profile-page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 import { DividerModule } from 'primeng/divider';
 import { PanelModule } from 'primeng/panel';
 
 @Component({
   selector: 'app-profile-page',
-  imports: [PanelModule, DividerModule],
+  imports: [PanelModule, DividerModule, TranslatePipe],
   templateUrl: './profile-page.html',
   styleUrl: './profile-page.css',
 })
