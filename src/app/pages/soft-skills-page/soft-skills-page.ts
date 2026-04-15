9ded9edc92a3f32b232f2c8fbf051969c0cb4615
Jacques Tea <jacquestea16@gmail.com>
2026-04-15 12:57:37 +0200

    soft-skills translation

--- src/app/pages/soft-skills-page/soft-skills-page.ts
+++ src/app/pages/soft-skills-page/soft-skills-page.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { _, TranslatePipe, TranslateService, Translation } from '@ngx-translate/core';
 import { PanelModule } from 'primeng/panel';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-soft-skills-page',
-  imports: [PanelModule],
+  imports: [PanelModule, TranslatePipe, AsyncPipe],
   templateUrl: './soft-skills-page.html',
   styleUrl: './soft-skills-page.css',
 })
-export class SoftSkillsPage {}
+export class SoftSkillsPage {
+  private translate: TranslateService = inject(TranslateService);
+
+  protected getSkills(): Observable<Translation> {
+    return this.translate.get(_('soft-skills.items'));
+  }
+}
