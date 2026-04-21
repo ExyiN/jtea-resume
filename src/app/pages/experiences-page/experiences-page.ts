f8d643e940495334242eb5a502378ba7913b1fb1
Jacques Tea <jacquestea16@gmail.com>
2026-04-21 10:26:37 +0200

    create component

---
+++ src/app/pages/experiences-page/experiences-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experiences-page',
+  imports: [],
+  templateUrl: './experiences-page.html',
+  styleUrl: './experiences-page.css',
+})
+export class ExperiencesPage {}
